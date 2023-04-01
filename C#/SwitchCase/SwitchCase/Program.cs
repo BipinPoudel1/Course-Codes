4555e9c94e44a248bfff8c2adadf62b4622eabb0
Bipin <xperiabpn@gmail.com>
2023-04-01 10:13:14 +0545

    Add: Switch Case Example

---
+++ C#/SwitchCase/SwitchCase/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SwitchCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            void weekEnd()
+            {
+                switch (DateTime.Now.DayOfWeek)
+                {
+                    case DayOfWeek.Saturday:
+                        Console.WriteLine("Today is Weekend.");
+                        break;
+
+                    default:
+                        Console.WriteLine("Today is working day.");
+                        break;
+                }
+            }
+            weekEnd();
+            Console.ReadKey();
+        }
+    }
+}
