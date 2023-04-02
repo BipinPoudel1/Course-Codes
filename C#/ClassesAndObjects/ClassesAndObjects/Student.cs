cb0655e99a5d66bdb8b00030c6ab32fccd37542d
Bipin <xperiabpn@gmail.com>
2023-04-02 17:45:21 +0545

    Added: Basic Concept of Class and Object

---
+++ C#/ClassesAndObjects/ClassesAndObjects/Student.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ClassesAndObjects
+{
+    internal class Student
+    {
+        int rollNo;
+        string name;
+        int age;
+
+        public void setStudent(int rollNo, string name, int age)
+        {
+            this.rollNo = rollNo;
+            this.name = name;
+            this.age = age;
+        }
+
+        public void getStudent()
+        {
+            Console.WriteLine("Roll No.: " + this.rollNo);
+            Console.WriteLine("Name: " + this.name);
+            Console.WriteLine("Age: " + this.age);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your Roll No. ");
+            int roll=Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            int ag = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\n");
+
+            Student Bipin = new Student();
+            Bipin.setStudent(roll, name, ag);
+
+            Student Aruna = new Student();
+            Aruna.setStudent(1, "Aruna", 22);
+            
+            Aruna.getStudent();
+
+            Bipin.getStudent();
+            Console.ReadKey();
+            
+        }
+    }
+}
