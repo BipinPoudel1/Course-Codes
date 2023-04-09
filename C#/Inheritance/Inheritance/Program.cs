dbc3b14d6e0774a90592eee7651f70a07a146843
Bipin <xperiabpn@gmail.com>
2023-04-09 18:24:36 +0545

    Added: Inheritance

---
+++ C#/Inheritance/Inheritance/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class VisitingEmployees : Employees
+    {
+        public int VisitingSalary;
+        public int VisitingHours;
+    }
+
+    class PermanentEmployees : Employees
+    {
+        public int PermanentSalary;
+        public int PermanentHours;
+    }
+
+    class Employees
+    {
+        public int EmpID;
+        public string EmpName;
+        public int EmpAge;
+        public int EmpContactNo;
+
+        public void show()
+        {
+            Console.WriteLine("This is a method of a parent (base) class.");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PermanentEmployees Bipin = new PermanentEmployees();
+            Bipin.EmpID = 12;
+            Bipin.show();
+
+            VisitingEmployees Jonna = new VisitingEmployees();
+            Jonna.EmpID = 23;
+
+            Console.WriteLine("ID: " + Bipin.EmpID);
+            Console.WriteLine("ID: " + Jonna.EmpID);
+
+            Console.ReadKey();
+        }
+    }
+}
