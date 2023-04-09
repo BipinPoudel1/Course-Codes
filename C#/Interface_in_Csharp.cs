c8a4fdf58d6bf364fb4af82f16b5257e28f8652c
Bipin <xperiabpn@gmail.com>
2023-04-09 19:37:18 +0545

    Added: Interface

---
+++ C#/Interface_in_Csharp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    interface IEmployee
+    {
+        void show();
+    }
+
+    class PartTimeEmplyees : IEmployee
+    {
+        public void show()
+        {
+            Console.WriteLine("This is a method of IEmployee interface.");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PartTimeEmplyees pte = new PartTimeEmplyees();
+            pte.show();
+
+            Console.ReadKey();
+        }
+    }
+}
