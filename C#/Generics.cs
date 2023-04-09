9ec1a05d6d29647b05f5c80210904e623b64c1f1
Bipin <xperiabpn@gmail.com>
2023-04-09 21:45:37 +0545

    Added: Generics

---
+++ C#/Generics.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Generics
+{
+    internal class Program
+    {
+        class Example
+        {
+            public static void ShowArray<T>(T[] arr)
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+            }
+
+            public static bool Check<T>(T a, T b)
+            {
+                bool c = a.Equals(b);
+                return c;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] Numbers = new int[3];
+            Numbers[0] = 11;
+            Numbers[1] = 22;
+            Numbers[2] = 33;
+
+            string[] Names = { "Bipin", "Rebeca", "Laura" };
+            double[] Pints = { 2.3, 4.6, 2.4 };
+
+            Example.ShowArray(Numbers);
+            Console.WriteLine("_____________________________");
+            Example.ShowArray(Names);
+            Console.WriteLine("_____________________________");
+            Example.ShowArray(Pints);
+            Console.WriteLine("_____________________________");
+
+            Console.WriteLine(Example.Check(10, 20));
+            Console.WriteLine(Example.Check("Leela", "Leela"));
+            Console.WriteLine(Example.Check('A', 'B'));
+            Console.ReadKey();
+        }
+    }
+}
